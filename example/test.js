672176e6ae7b47bcee02f97acfbc94c149b80fae
Mookky <dusadee@iapp.co.th>
2020-02-18 18:25:48 +0700

    add modulePathIgnorePatterns on jestconfig

--- example/test.js
+++ example/test.js
@@ -3,12 +3,12 @@ const { CheckTaxID } = require('../lib/index')
 const TAXID = ""
 CheckTaxID(TAXID)
 .then(result => {
-  console.log('result1:',result)
+  // console.log('result1:',result)
 })
 
 async function getData() {
   let result = await CheckTaxID(TAXID)
-  console.log('result2:',result)
+  // console.log('result2:',result)
 }
 
 getData()
